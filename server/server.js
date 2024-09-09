07528436ea8cb26a2ecab1161c9d8d288e6513dc
David Vinson <davidvinson018@gmail.com>
2024-09-09 10:02:30 -0500

    built out POST route to handle add, subtrack, multi, div ops

--- server/server.js
+++ server/server.js
@@ -19,9 +19,37 @@ app.get('/calculations', (req, res) => {
 });
 
 // POST /calculations
+app.post('/calculations', (req, res) => {
+  //what will our req.body look like? { numOne: 25, numTwo: 10, operator: '+' }
+  console.log('req.body', req.body);
+  let obj2 = {
+    numOne: req.body.numOne,
+    numTwo: req.body.numTwo,
+    operator: req.body.operator,
+    result: 0,
+  };
+  if (req.body.operator === '+') {
+    //add req.body.numOne and req.body.numTwo
+    obj2.result = Number(req.body.numOne) + Number(req.body.numTwo);
+  } else if (req.body.operator === '-') {
+    obj2.result = Number(req.body.numOne) - Number(req.body.numTwo);
+  } else if (req.body.operator === '*') {
+    obj2.result = Number(req.body.numOne) * Number(req.body.numTwo);
+    // res.status(201);
+    //jacob
+  } else if (req.body.operator === '/') {
+    obj2.result = Number(req.body.numOne) / Number(req.body.numTwo);
+    //jenny
+  }
+
+  console.log('calc obj', obj2);
+  calculations.push(obj2);
+
+  res.sendStatus(201); //201 created
+});
 
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
-// 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
+// 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸 🐻‍❄️ 🐼 🐨 🐨 🐼 🐼 📉 <(bear market)
 
 // Makes it so you don't have to kill the server
 // on 5000 in order to run the tests:
